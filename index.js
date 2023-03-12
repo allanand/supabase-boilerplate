efea4a01b9d8f55d43f9dced92c278af3aa88fd3
allanand <112962855+allanand@users.noreply.github.com>
2023-03-12 15:58:47 -0400

    test 3

---
+++ index.js
@@ -0,0 +1,20 @@
+
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.8.0"
+
+const supabaseUrl = 'https://ijxddosmmvfqkuijlxdr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqeGRkb3NtbXZmcWt1aWpseGRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzc0NDQzNTksImV4cCI6MTk5MzAyMDM1OX0.tJPbWMwvU-GL-o47jbgsf0nS3lHLiTbzWgyiUuTAiSE'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getbooks() {
+  let { data: books, error } = await supabase
+  .from('books')
+  .select('*')
+
+for (let book of books) {
+  let booklist = document.getElementById('books');
+  booklist.innerHTML += `<li>${book.title} - ${book.author} - ${book.isbn} </li>`;
+  }
+  
+}
+
+getbooks();
